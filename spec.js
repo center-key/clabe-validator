7963ae061c32eb53fe5a6337cb98a59cddcef85f
Dem Pilafian <dem@centerkey.com>
2017-01-22 21:49:35 -0800

    Test case to find duplicate cities
    Issue #2
    
    Failures:
    1) List of CLABE cities has no duplicate city names.
    1.1) Expected [ 27, 'Tijuana' ] to be 'unique -- see: 28'.
    1.2) Expected [ 135, 'Tonala' ] to be 'unique -- see: 320'.
    1.3) Expected [ 370, 'Ocotlán' ] to be 'unique -- see: 622'.
    1.4) Expected [ 404, 'Túxpam' ] to be 'unique -- see: 575'.

--- spec.js
+++ spec.js
@@ -3,7 +3,7 @@
 var clabe = require('./clabe.js');
 
 describe('List of CLABE banks', () => {
-   it('contains the correct code to look up a bank name.', () => {
+   it('contains the correct code to look up a bank name', () => {
       var dataSet = [
          { input: '002', expected: 'Banco Nacional de México' },
          { input: '640', expected: 'J.P. Morgan Casa de Bolsa' }
@@ -19,7 +19,7 @@ describe('List of CLABE cities', () => {
    var cityMap = {};
    function addCity(city) { cityMap[city[1]] = city[0]; }
    clabe.cities.forEach(addCity);
-   it('contains the correct code for a city.', () => {
+   it('contains the correct code for a city', () => {
       var dataSet = [
          { input: 'Tecate',              expected: '027' },
          { input: 'La Mesa',             expected: '028' },
@@ -31,10 +31,17 @@ describe('List of CLABE cities', () => {
          }
       dataSet.forEach(evalData);
       });
+   it('has no duplicate city names', () => {
+      function checkForDuplicate(city) {
+         if (cityMap[city[1]] !== city[0])
+            expect(city).toBe('unique -- see: ' + cityMap[city[1]]);
+         }
+      clabe.cities.forEach(checkForDuplicate);
+      });
    });
 
 describe('CLABE validator', () => {
-   it('rejects an invalid CLABE number.', () => {
+   it('rejects an invalid CLABE number', () => {
       var dataSet = [
          { input: '002010077777777779', expected: 'Invalid checksum, last digit should be: 1' },
          { input: '000000000000000000', expected: 'Invalid bank code' },
@@ -45,10 +52,11 @@ describe('CLABE validator', () => {
          }
       dataSet.forEach(evalData);
       });
-   it('accepts a valid CLABE number.', () => {
+   it('accepts a valid CLABE number', () => {
       var dataSet = [
          '002010077777777771',
          '032180000118359719',
+         '014027000005555558',
          '014028000005555557'
          ];
       function evalData(data) {
