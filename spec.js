1bc897554d37762964f1af7acb8c7d50985d35fe
Dem Pilafian <dem@centerkey.com>
2017-05-19 20:34:17 -0700

    es6 declarations

--- spec.js
+++ spec.js
@@ -5,14 +5,14 @@
 //    $ cd clabe-validator
 //    $ npm test
 
-var assert = require('assert');
-var clabe =  require('./clabe.js');
+const assert = require('assert');
+const clabe =  require('./clabe.js');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('List of CLABE banks', () => {
 
    it('contains the correct code to look up a bank name', () => {
-      var dataSet = [
+      const dataSet = [
          { input: '002', expected: 'Banco Nacional de México' },
          { input: '640', expected: 'J.P. Morgan Casa de Bolsa' }
          ];
@@ -26,12 +26,12 @@ describe('List of CLABE banks', () => {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('List of CLABE cities', () => {
-   var cityMap = {};
+   let cityMap = {};
    function addCity(city) { cityMap[city[1]] = city[0]; }
    clabe.cities.forEach(addCity);
 
    it('contains the correct code for a city', () => {
-      var dataSet = [
+      const dataSet = [
          { input: 'Tecate',              expected: '027' },
          { input: 'La Mesa',             expected: '028' },
          { input: 'Rosarito',            expected: '028' },
@@ -57,7 +57,7 @@ describe('List of CLABE cities', () => {
 describe('CLABE validator', () => {
 
    it('rejects an invalid CLABE number', () => {
-      var dataSet = [
+      const dataSet = [
          { input: '002010077777777779', expected: 'Invalid checksum, last digit should be: 1' },
          { input: '000000000000000000', expected: 'Invalid bank code' },
          { input: '002115016003269411', expected: 'Invalid city code' }
@@ -69,7 +69,7 @@ describe('CLABE validator', () => {
       });
 
    it('accepts a valid CLABE number', () => {
-      var dataSet = [
+      const dataSet = [
          '002010077777777771',
          '032180000118359719',
          '014027000005555558',
@@ -87,14 +87,14 @@ describe('CLABE validator', () => {
 describe('CLABE calculator', () => {
 
    it('builds the correct CLABE number', () => {
-      var dataSet = [
+      const dataSet = [
          { input: { bank:  2, city:  10, acct: 7777777777 }, expected: '002010077777777771' },
          { input: { bank: 32, city: 180, acct:   11835971 }, expected: '032180000118359719' },
          { input: { bank: 14, city:  27, acct:     555555 }, expected: '014027000005555558' },
          { input: { bank: 14, city:  28, acct:     555555 }, expected: '014028000005555557' }
          ];
       function evalData(data) {
-         var clabeNum = clabe.calculate(data.input.bank, data.input.city, data.input.acct);
+         const clabeNum = clabe.calculate(data.input.bank, data.input.city, data.input.acct);
          assert.equal(clabeNum, data.expected);
          }
       dataSet.forEach(evalData);
