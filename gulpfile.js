d4cf77efee438001f258c88b5ed81cc80d7d36f7
Dem Pilafian <dem@centerkey.com>
2018-10-19 22:49:03 -0700

    Switch to gulp-w3c-html-validator to fix security vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@
 // MIT License
 
 // Imports
-const gulp =             require('gulp');
-const header =           require('gulp-header');
-const htmlHint =         require('gulp-htmlhint');
-const jshint =           require('gulp-jshint');
-const rename =           require('gulp-rename');
-const replace =          require('gulp-replace');
-const size =             require('gulp-size');
-const uglify =           require('gulp-uglify');
-const w3cHtmlValidator = require('gulp-w3cjs');
+const gulp =          require('gulp');
+const header =        require('gulp-header');
+const htmlHint =      require('gulp-htmlhint');
+const htmlValidator = require('gulp-w3c-html-validator');
+const jshint =        require('gulp-jshint');
+const rename =        require('gulp-rename');
+const replace =       require('gulp-replace');
+const size =          require('gulp-size');
+const uglify =        require('gulp-uglify');
 
 // Setup
 const pkg =            require('./package.json');
@@ -25,10 +25,10 @@ const jsHintConfig =   { strict: 'implied', undef: true, unused: true, browser: 
 const task = {
    analyzeHtml: function() {
       return gulp.src('*.html')
-         .pipe(w3cHtmlValidator())
-         .pipe(w3cHtmlValidator.reporter())
          .pipe(htmlHint(htmlHintConfig))
-         .pipe(htmlHint.reporter());
+         .pipe(htmlHint.reporter())
+         .pipe(htmlValidator())
+         .pipe(htmlValidator.reporter());
       },
    analyzeJs: function() {
       return gulp.src('clabe.js')
