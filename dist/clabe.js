164531e901c6502e85598d967007dcfc3f9c5649
Dem Pilafian <dem@centerkey.com>
2019-02-18 23:55:46 -0800

    Dependencies updated

--- dist/clabe.js
+++ dist/clabe.js
@@ -8,10 +8,9 @@ const clabe = {
       // Returns the checksum calculated from the first 17 characters of CLABE number.
       // Example:
       //    const checksum = clabe.computeChecksum('00201007777777777');  //value: 1
-      let sum = 0;
-      const add = (digit, index) => sum += (parseInt(digit) * [3, 7, 1][index % 3]) % 10;
-      clabeNum17.split('').slice(0, 17).forEach(add);
-      return (10 - (sum % 10)) % 10;
+      const add = (sum, digit, i) => sum + (parseInt(digit) * [3, 7, 1][i % 3]) % 10;
+      const compute = () => (10 - (clabeNum17.split('').slice(0, 17).reduce(add, 0) % 10)) % 10;
+      return /^[0-9]{17,18}$/.test(clabeNum17) ? compute() : null;
       },
 
    validate: (clabeNum) => {
@@ -41,8 +40,8 @@ const clabe = {
          makeCitiesMap();
       const bank = clabe.banksMap[parseInt(bankCode)] || {};
       const city = clabe.citiesMap[parseInt(cityCode)];
+      const realChecksum = clabe.computeChecksum(clabeNum);
       const getValidationInfo = () => {
-         const realChecksum = clabe.computeChecksum(clabeNum);
          const validationInfo =
             clabeNum.length !== 18 ?    { invalid: 'length',     data: '' } :
             /[^0-9]/.test(clabeNum) ?   { invalid: 'characters', data: '' } :
@@ -63,7 +62,8 @@ const clabe = {
          bank:     bank.name,
          city:     city,
          account:  account,
-         code:     { bank: bankCode, city: cityCode }
+         code:     { bank: bankCode, city: cityCode },
+         checksum: realChecksum
          };
       },
 
