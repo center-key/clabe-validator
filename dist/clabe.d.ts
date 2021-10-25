b1fbdd45688cdb8beb9ecb71e537bfdfd073032c
dpilafian <dem@centerkey.com>
2021-10-24 18:57:17 -0700

    Release v1.7.2

--- dist/clabe.d.ts
+++ dist/clabe.d.ts
@@ -1,4 +1,4 @@
-//! clabe-validator v1.7.1 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
+//! clabe-validator v1.7.2 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
 
 export declare type ClabeBank = {
     tag?: string;
--- dist/clabe.js
+++ dist/clabe.js
@@ -1,7 +1,7 @@
-//! clabe-validator v1.7.1 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
+//! clabe-validator v1.7.2 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
 
 const clabe = {
-    version: '1.7.1',
+    version: '1.7.2',
     computeChecksum(clabeNum17) {
         const x = (i) => [3, 7, 1][i % 3];
         const add = (sum, digit, i) => sum + (Number(digit) * x(i)) % 10;
