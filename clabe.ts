abec9abe93ad317d208864ef91522105ded2992f
dpilafian <dem@centerkey.com>
2022-11-08 01:42:06 -0800

    Bump tsc target to es2016

--- clabe.ts
+++ clabe.ts
@@ -27,7 +27,7 @@ export type ClabeMxState = 'MX-AGU' | 'MX-BCN' | 'MX-BCS' | 'MX-CAM' | 'MX-CHH' 
 
 const clabe = {
 
-   version: '~~~version~~~',
+   version: '{{pkg.version}}',
 
    computeChecksum(clabeNum17: string): number | null {
       // Returns the checksum calculated from the first 17 characters of CLABE number.
