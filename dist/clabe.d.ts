990e5d532e333860a9948497a0d56079b7f71371
dpilafian <dem@centerkey.com>
2022-11-20 01:10:43 -0800

    Release v2.1.1

--- dist/clabe.d.ts
+++ dist/clabe.d.ts
@@ -1,17 +1,17 @@
-//! clabe-validator v2.1.0 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
+//! clabe-validator v2.1.1 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
 
-export declare type ClabeBank = {
+export type ClabeBank = {
     tag?: string;
     name?: string;
 };
-export declare type ClabeBanksMap = {
+export type ClabeBanksMap = {
     [bankCode: number]: ClabeBank;
 };
-export declare type ClabeCityInfo = [number, string, ClabeMxState?];
-export declare type ClabeCitiesMap = {
+export type ClabeCityInfo = [number, string, ClabeMxState?];
+export type ClabeCitiesMap = {
     [cityCode: number]: ClabeCityInfo[];
 };
-export declare type ClabeCheck = {
+export type ClabeCheck = {
     ok: boolean;
     formatOk: boolean;
     error: string | null;
@@ -29,7 +29,7 @@ export declare type ClabeCheck = {
     };
     checksum: number | null;
 };
-export declare type ClabeMxState = 'MX-AGU' | 'MX-BCN' | 'MX-BCS' | 'MX-CAM' | 'MX-CHH' | 'MX-CHP' | 'MX-CMX' | 'MX-COA' | 'MX-COL' | 'MX-DUR' | 'MX-GRO' | 'MX-GUA' | 'MX-HID' | 'MX-JAL' | 'MX-MEX' | 'MX-MIC' | 'MX-MOR' | 'MX-NAY' | 'MX-NLE' | 'MX-OAX' | 'MX-PUE' | 'MX-QUE' | 'MX-ROO' | 'MX-SIN' | 'MX-SLP' | 'MX-SON' | 'MX-TAB' | 'MX-TAM' | 'MX-TLA' | 'MX-VER' | 'MX-YUC' | 'MX-ZAC';
+export type ClabeMxState = 'MX-AGU' | 'MX-BCN' | 'MX-BCS' | 'MX-CAM' | 'MX-CHH' | 'MX-CHP' | 'MX-CMX' | 'MX-COA' | 'MX-COL' | 'MX-DUR' | 'MX-GRO' | 'MX-GUA' | 'MX-HID' | 'MX-JAL' | 'MX-MEX' | 'MX-MIC' | 'MX-MOR' | 'MX-NAY' | 'MX-NLE' | 'MX-OAX' | 'MX-PUE' | 'MX-QUE' | 'MX-ROO' | 'MX-SIN' | 'MX-SLP' | 'MX-SON' | 'MX-TAB' | 'MX-TAM' | 'MX-TLA' | 'MX-VER' | 'MX-YUC' | 'MX-ZAC';
 declare const clabe: {
     version: string;
     computeChecksum(clabeNum17: string): number | null;
