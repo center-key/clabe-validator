e09d4279d0b843e3205d9ed0f7d43b35e02cc0e2
dpilafian <dem@centerkey.com>
2023-01-25 03:10:38 -0800

    Release v2.1.3

--- dist/clabe.dev.js
+++ dist/clabe.dev.js
@@ -1,7 +1,7 @@
-//! clabe-validator v2.1.2 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
+//! clabe-validator v2.1.3 ~~ https://github.com/center-key/clabe-validator ~~ MIT License
 
 const clabe = {
-    version: '2.1.2',
+    version: '2.1.3',
     computeChecksum(clabeNum17) {
         const x = (i) => [3, 7, 1][i % 3];
         const add = (sum, digit, i) => sum + (Number(digit) * x(i)) % 10;
